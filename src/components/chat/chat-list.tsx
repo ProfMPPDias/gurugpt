d70763b4c8726c64f71cfd9a8c577163a7588461
Marcos Paulo Pereira Dias <145621548+ProfMPPDias@users.noreply.github.com>
2025-08-21 00:36:42 -0300

    v2.0
    Updated

--- src/components/chat/chat-list.tsx
+++ src/components/chat/chat-list.tsx
@@ -39,7 +39,7 @@ export default function ChatList({
         {loadingSubmit && (
           <ChatBubble variant="received">
             <ChatBubbleAvatar
-              src="ollama.png"
+              src="/ollama.png"
               width={6}
               height={6}
               className="object-contain dark:invert"
